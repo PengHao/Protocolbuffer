7d6a9dfe8a358c161db07b389267c20edf31d5da
彭浩 <wolfpeng.ph@alibaba-inc.com>
2017-11-08 20:17:50 +0800

    delete no use code

--- ProtocolBuffer/ProtocolBuffer.h
+++ ProtocolBuffer/ProtocolBuffer.h
@@ -1,13 +1,19 @@
 //
-//  ProtocolBuffer.h
-//  ProtocolBuffer
+//  protocolbuffer.h
+//  protocolbuffer
 //
-//  Created by 彭浩 on 16/7/25.
-//  Copyright © 2016年 wolfpeng. All rights reserved.
+//  Created by peng hao on 2017/11/8.
+//  Copyright © 2017年 wolfpeng. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface ProtocolBuffer : NSObject
-- (void) test;
-@end
+//! Project version number for protocolbuffer.
+FOUNDATION_EXPORT double protocolbufferVersionNumber;
+
+//! Project version string for protocolbuffer.
+FOUNDATION_EXPORT const unsigned char protocolbufferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <protocolbuffer/PublicHeader.h>
+
+
